e7559589918686e9975a28845019650fac0f2d39
Kemas Nayar <kem4s2422@gmail.com>
2025-11-29 19:06:09 +0700

    Update index.php
    penampilan link

--- klinik_prestasi/karya/index.php
+++ klinik_prestasi/karya/index.php
@@ -12,8 +12,9 @@ include '../layout/header.php';
         <tr>
             <th>ID Karya</th>
             <th>Judul Karya</th>
-            <th>Penulis (ID User)</th>
+            <th>Penulis</th>
             <th>Bidang</th>
+            <th>Link Drive</th> <!-- Label diperbarui -->
             <th>Aksi</th>
         </tr>
     </thead>
@@ -24,14 +25,23 @@ include '../layout/header.php';
 
         while ($row = pg_fetch_assoc($result)) {
             $row = array_change_key_case($row, CASE_UPPER);
+            
+            // Logika Link Google Drive
+            $file_link = "-";
+            if (!empty($row['FILE_KARYA'])) {
+                // Langsung gunakan isi kolom sebagai href karena isinya adalah URL
+                $file_link = "<a href='" . htmlspecialchars($row['FILE_KARYA']) . "' target='_blank' style='color:blue; text-decoration:underline;'>Buka Link</a>";
+            }
+
             echo "<tr>";
             echo "<td>" . htmlspecialchars($row['ID_KARYA']) . "</td>";
             echo "<td>" . htmlspecialchars($row['JUDUL_KARYA']) . "</td>";
             echo "<td>" . htmlspecialchars($row['ID_USER']) . "</td>";
             echo "<td>" . htmlspecialchars($row['BIDANG_KARYA']) . "</td>";
+            echo "<td>" . $file_link . "</td>";
             echo "<td>
                     <a href='edit.php?id=" . $row['ID_KARYA'] . "' class='btn btn-blue'>Edit</a>
-                    <a href='delete.php?id=" . $row['ID_KARYA'] . "' class='btn btn-red' onclick=\"return confirm('Hapus karya " . $row['JUDUL_KARYA'] . "?');\">Hapus</a>
+                    <a href='delete.php?id=" . $row['ID_KARYA'] . "' class='btn btn-red' onclick=\"return confirm('Hapus karya ini?');\">Hapus</a>
                   </td>";
             echo "</tr>";
         }
